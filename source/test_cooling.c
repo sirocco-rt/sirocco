72eefbd4a9e3a530c13326e9dbd2464ba45c0969
Knox S. Long <long@stsci.edu>
2025-09-05 06:55:09 -0400

    Partial update

--- source/test_cooling.c
+++ source/test_cooling.c
@@ -28,6 +28,7 @@
 #include "sirocco.h"
 
 
+
 char inroot[LINELENGTH], outroot[LINELENGTH], model_file[LINELENGTH], folder[LINELENGTH];
 int model_flag, ksl_flag, cmf2obs_flag, obs2cmf_flag;
 
@@ -155,6 +156,88 @@ xparse_command_line (argc, argv)
 
 
 
+/* An externall pointer reference used by zero_emit.  */
+PlasmaPtr xxxplasma;
+
+
+
+double
+xcalc_te (PlasmaPtr xplasma, double tmin, double tmax)
+{
+  double z1, z2;
+  int ierr = FALSE;
+
+
+  /* we assign a plasma pointer here to a fixed structure because
+   * we need to call zbrent and we cannot pass the xplasma ptr directly
+   */
+
+  xxxplasma = xplasma;
+
+  xxxplasma->heat_tot += xxxplasma->heat_ch_ex;
+
+  xplasma->t_e = tmin;
+  z1 = zero_emit (tmin);
+  xplasma->t_e = tmax;
+  z2 = zero_emit (tmax);
+
+  /* The way this works is that if we have a situation where the cooling
+   * at tmax and tmin brackets the heating, then we use zbrent to improve
+   * the estimated temperature, but if not we chose the best direction
+   */
+
+  if ((z1 * z2 < 0.0))
+  {                             // Then the interval is bracketed
+    xplasma->t_e = zero_find (zero_emit2, tmin, tmax, 50., &ierr);
+    if (ierr)
+    {
+      Error ("calc_te: zero_find failed to find a temperature\n");
+    }
+
+  }
+  else if (fabs (z1) < fabs (z2))
+  {
+    xplasma->t_e = tmin;
+  }
+  else
+  {
+    xplasma->t_e = tmax;
+  }
+  /* With the new temperature in place for the cell, get the correct value of heat_tot.
+     SS June  04 */
+
+  /* At this point we know the temperature that balances heating and cooling
+   * within the constraints set by tmin and tmax.
+   */
+
+
+  /* Update heat_tot and heat_lines for macro_bb_heating at the new temperature. 
+   * We subtract the current value and then compute at the new temperature and
+   * add this back */
+
+  xplasma->heat_tot -= xplasma->heat_lines_macro;
+  xplasma->heat_lines -= xplasma->heat_lines_macro;
+
+  xplasma->heat_lines_macro = macro_bb_heating (xplasma, xplasma->t_e);
+
+  xplasma->heat_tot += xplasma->heat_lines_macro;
+  xplasma->heat_lines += xplasma->heat_lines_macro;
+
+  /* Similaryly for macro_atom_bf_heating */
+
+  xplasma->heat_tot -= xplasma->heat_photo_macro;
+  xplasma->heat_photo -= xplasma->heat_photo_macro;
+
+  xplasma->heat_photo_macro = macro_bf_heating (xplasma, xplasma->t_e);
+
+  xplasma->heat_tot += xplasma->heat_photo_macro;
+  xplasma->heat_photo += xplasma->heat_photo_macro;
+
+
+  return (xplasma->t_e);
+
+}
+
 /**********************************************************/
 /**
  * @brief      the main routine which carries out the effort
@@ -218,353 +301,19 @@ main (argc, argv)
 
   fprintf (fptr, "# Results for %s\n", infile);
 
-  fprintf (fptr, "# Size  %d  %d %d \n", size_Jbar_est, size_gamma_est, size_alpha_est);
 
-  fprintf (fptr, "%15s %4s %4s %4s ", "Variable", "np", "i", "j");
-  for (i = 0; i < nlevels_macro; i++)
-  {
-    fprintf (fptr, "MacLev%02d ", i);
-  }
-  fprintf (fptr, "\n");
+  printf ("te %.3e\n", plasmamain[0].t_e);
+  fprintf (fptr, "te %.3e\n", plasmamain[0].t_e);
 
-  fprintf (fptr, "%15s %4s %4s %4s ", "---------------", "----", "----", "----");
-  for (i = 0; i < nlevels_macro; i++)
-  {
-    fprintf (fptr, "-------- ");
-  }
-  fprintf (fptr, "\n");
+  double t, t_new;
+  t = plasmamain[0].t_e;
 
-  for (n = 0; n < NPLASMA; n++)
-  {
+  t_new = xcalc_te (&plasmamain[0], 0.7 * t, 1.3 * t);
+  printf ("te_new %.3e\n", t_new);
+  fprintf (fptr, "te_new %.3e\n", t_new);
 
-    fprintf (fptr, "%15s", "jbar");
-
-    nnwind = plasmamain[n].nwind;
-    ndom = wmain[nnwind].ndom;
-    wind_n_to_ij (ndom, nnwind, &ii, &jj);
-    fprintf (fptr, " %4d %4d %4d ", n, ii, jj);
-
-    for (i = 0; i < nlevels_macro; i++)
-    {
-      fprintf (fptr, "%8.2e ", macromain[n].jbar[i]);
-    }
-    fprintf (fptr, "\n");
-  }
-
-
-
-  for (n = 0; n < NPLASMA; n++)
-  {
-    fprintf (fptr, "%15s", "jbar_old");
-
-    nnwind = plasmamain[n].nwind;
-    ndom = wmain[nnwind].ndom;
-    wind_n_to_ij (ndom, nnwind, &ii, &jj);
-    fprintf (fptr, " %4d %4d %4d ", n, ii, jj);
-
-    for (i = 0; i < nlevels_macro; i++)
-    {
-      fprintf (fptr, "%8.2e ", macromain[n].jbar_old[i]);
-    }
-    fprintf (fptr, "\n");
-  }
-
-
-
-  for (n = 0; n < NPLASMA; n++)
-  {
-    fprintf (fptr, "%15s", "gamma");
-
-    nnwind = plasmamain[n].nwind;
-    ndom = wmain[nnwind].ndom;
-    wind_n_to_ij (ndom, nnwind, &ii, &jj);
-    fprintf (fptr, " %4d %4d %4d ", n, ii, jj);
-
-    for (i = 0; i < nlevels_macro; i++)
-    {
-      fprintf (fptr, "%8.2e ", macromain[n].gamma[i]);
-    }
-    fprintf (fptr, "\n");
-  }
-
-
-
-
-  for (n = 0; n < NPLASMA; n++)
-  {
-    fprintf (fptr, "%15s", "alpha_st");
-
-    nnwind = plasmamain[n].nwind;
-    ndom = wmain[nnwind].ndom;
-    wind_n_to_ij (ndom, nnwind, &ii, &jj);
-    fprintf (fptr, " %4d %4d %4d ", n, ii, jj);
-
-    for (i = 0; i < nlevels_macro; i++)
-    {
-      fprintf (fptr, "%8.2e ", macromain[n].alpha_st[i]);
-    }
-    fprintf (fptr, "\n");
-  }
-
-
-
-
-  for (n = 0; n < NPLASMA; n++)
-  {
-    fprintf (fptr, "%15s", "recomb_sp");
-
-    nnwind = plasmamain[n].nwind;
-    ndom = wmain[nnwind].ndom;
-    wind_n_to_ij (ndom, nnwind, &ii, &jj);
-    fprintf (fptr, " %4d %4d %4d ", n, ii, jj);
-
-    for (i = 0; i < nlevels_macro; i++)
-    {
-      fprintf (fptr, "%8.2e ", macromain[n].recomb_sp[i]);
-    }
-    fprintf (fptr, "\n");
-  }
-
-
-
-
-  for (n = 0; n < NPLASMA; n++)
-  {
-    fprintf (fptr, "%15s", "matom_abs");
-
-    nnwind = plasmamain[n].nwind;
-    ndom = wmain[nnwind].ndom;
-    wind_n_to_ij (ndom, nnwind, &ii, &jj);
-    fprintf (fptr, " %4d %4d %4d ", n, ii, jj);
-
-    for (i = 0; i < nlevels_macro; i++)
-    {
-      fprintf (fptr, "%8.2e ", macromain[n].matom_abs[i]);
-    }
-    fprintf (fptr, "\n");
-  }
-
-
-
-
-  for (n = 0; n < NPLASMA; n++)
-  {
-    fprintf (fptr, "%15s", "matom_emiss");
-
-    nnwind = plasmamain[n].nwind;
-    ndom = wmain[nnwind].ndom;
-    wind_n_to_ij (ndom, nnwind, &ii, &jj);
-    fprintf (fptr, " %4d %4d %4d ", n, ii, jj);
-
-    for (i = 0; i < nlevels_macro; i++)
-    {
-      fprintf (fptr, "%8.2e ", macromain[n].matom_emiss[i]);
-    }
-    fprintf (fptr, "\n");
-  }
   fclose (fptr);
-
-
-  /* calculate all the line luminosities in macro-atom mode */
-  /* these are stored in a folder with a separate file for each upper level */
-  sprintf (folder, "matom_linelum_%.200s", inroot);
-  mkdir (folder, 0777);
-  printf ("Calculating macro-atom line luminosities for all lines in wavelength range %.1f to %.1f Angstroms...\n",
-          VLIGHT / geo.sfmax / ANGSTROM, VLIGHT / geo.sfmin / ANGSTROM);
-
-  /* create a file that tells the user the wavelengths of every macro-atom line and the 
-     corresponding upper and lower levels */
-  create_matom_level_map ();
-
-  /* loop over all macro atom upper levels */
-  for (i = 0; i < nlevels_macro; i++)
-  {
-    line_matom_lum (i);
-  }
-  printf ("Done for %d levels.\n", nlevels_macro);
 
   exit (0);
 
-}
-
-/**********************************************************/
-/**
- * @brief print out which line wavelengths correspond to which upper and lower levels 
- *
- **********************************************************/
-
-int
-create_matom_level_map ()
-{
-  int uplvl, nbbd, n;
-  char outfile[LINELENGTH];
-  FILE *fptr, *fopen ();
-
-  /* open a file in the folder where we store the matom line luminosities */
-  sprintf (outfile, "%.200s/line_map.txt", folder);
-
-  /* print some header information to the file */
-  fptr = fopen (outfile, "w");
-  fprintf (fptr, "# model name %s\n", inroot);
-  fprintf (fptr, "# which line wavelengths (Angstroms) correspond to which upper and lower levels\n");
-  fprintf (fptr, "upper lower wavelength\n");
-
-  for (uplvl = 0; uplvl < nlevels_macro; uplvl++)
-  {
-    nbbd = xconfig[uplvl].n_bbd_jump;
-    for (n = 0; n < nbbd; n++)
-    {
-      fprintf (fptr, "%d %d %12.2f\n", uplvl, line[xconfig[uplvl].bbd_jump[n]].nconfigl,
-               VLIGHT / line[xconfig[uplvl].bbd_jump[n]].freq / ANGSTROM);
-    }
-  }
-  fclose (fptr);
-  return (0);
-}
-
-
-/**********************************************************/
-/**
- * @brief calculate line luminosities for all cells for a given upper level and save to file
- *
- **********************************************************/
-
-int
-line_matom_lum (uplvl)
-     int uplvl;
-{
-  int n, nbbd, i, ii, jj, nnwind, ndom, inwind;
-  double lum[NBBJUMPS];
-  char outfile[LINELENGTH];
-  FILE *fptr, *fopen ();
-
-  nbbd = xconfig[uplvl].n_bbd_jump;
-
-  /* open a file in the folder where we store the matom line luminosities */
-  sprintf (outfile, "%.100s/linelums_%.100s_lev%d.txt", folder, inroot, uplvl);
-
-  /* print some header information to the file */
-  fptr = fopen (outfile, "w");
-  fprintf (fptr, "# model name %s\n", inroot);
-  fprintf (fptr, "# Line luminosities from upper level %d from %.2f to %.2f Angstroms\n", uplvl, VLIGHT / geo.sfmax / ANGSTROM,
-           VLIGHT / geo.sfmin / ANGSTROM);
-  fprintf (fptr, "# Lower Levels:     ");
-  for (n = 0; n < nbbd; n++)
-  {
-    fprintf (fptr, "%12d", line[xconfig[uplvl].bbd_jump[n]].nconfigl);
-  }
-  fprintf (fptr, "\n# Line Wavelengths:");
-  for (n = 0; n < nbbd; n++)
-  {
-    fprintf (fptr, " %12.2f", VLIGHT / line[xconfig[uplvl].bbd_jump[n]].freq / ANGSTROM);
-  }
-  fprintf (fptr, "\n");
-
-  /* columns of wind cells */
-  fprintf (fptr, "%12s %12s %12s %4s %4s %12s %12s %12s", "nwind", "x", "z", "i", "j", "inwind", "wind_vol", "plasma_vol");
-
-  /* header info for each lower level */
-  for (n = 0; n < nbbd; n++)
-  {
-    fprintf (fptr, "  LowerLev%03d  ", line[xconfig[uplvl].bbd_jump[n]].nconfigl);
-  }
-  fprintf (fptr, "\n");
-  // fprintf (fptr, "%12s %12s %12s %4s %4s %12s %12s %12s", "------------", "------------", "------------", "----", "----", "------------",
-  //          "------------", "------------");
-  // for (n = 0; n < nbbd; n++)
-  // {
-  //   fprintf (fptr, "  -----------  ");
-  // }
-  // fprintf (fptr, "\n");
-
-
-  /* loop over each plasma cell and do the calculation */
-  for (nnwind = 0; nnwind < NDIM2; nnwind++)
-  {
-    ndom = wmain[nnwind].ndom;
-    inwind = wmain[nnwind].inwind;
-    wind_n_to_ij (ndom, nnwind, &ii, &jj);
-    fprintf (fptr, "%12d %12.4e %12.4e %4d %4d %12d %12.4e", nnwind, wmain[nnwind].xcen[0], wmain[nnwind].xcen[2], ii, jj, inwind,
-             wmain[nnwind].vol);
-    if (wmain[nnwind].inwind >= 0)
-    {
-      n = wmain[nnwind].nplasma;
-      line_matom_lum_single (lum, &plasmamain[n], uplvl);
-      /* print the filled volume */
-      fprintf (fptr, " %13.4e", plasmamain[n].vol);
-      for (i = 0; i < nbbd; i++)
-        fprintf (fptr, " %13.4e", lum[i]);
-    }
-    else
-    {
-      /* print a 0.0 instead of the filled volume and line lums outside the wind */
-      fprintf (fptr, " %13.4e", 0.0);
-      for (i = 0; i < nbbd; i++)
-        fprintf (fptr, " %13.4e", 0.0);
-    }
-
-    fprintf (fptr, "\n");
-  }
-  fclose (fptr);
-  return (0);
-}
-
-/**********************************************************/
-/**
- * @brief calculate line luminosities for a single cell for a given upper level 
- *
- **********************************************************/
-
-double
-line_matom_lum_single (lum, xplasma, uplvl)
-     double lum[];
-     PlasmaPtr xplasma;
-     int uplvl;
-{
-  int n, nbbd, m;
-  double penorm, bb_cont;
-  double freq_min, freq_max, lum_tot;
-  struct lines *line_ptr;
-  double eprbs[NBBJUMPS];
-  freq_min = geo.sfmin;
-  freq_max = geo.sfmax;
-  /* identify number of bb downward jumps */
-  nbbd = xconfig[uplvl].n_bbd_jump;
-  penorm = 0.0;
-  m = 0;
-  lum_tot = 0.0;
-  /* work out how often we come out in each of these locations */
-  for (n = 0; n < nbbd; n++)
-  {
-    line_ptr = &line[xconfig[uplvl].bbd_jump[n]];
-    if ((line_ptr->freq > freq_min) && (line_ptr->freq < freq_max))     // correct range
-    {
-      // bb_cont = (a21 (line_ptr) * p_escape (line_ptr, xplasma));
-      bb_cont = (a21 (line_ptr) * 1.0);
-      eprbs[m] = bb_cont * (xconfig[uplvl].ex - xconfig[line[xconfig[uplvl].bbd_jump[n]].nconfigl].ex); //energy difference
-      penorm += eprbs[m];
-    }
-    else
-    {
-      eprbs[m] = 0.0;
-    }
-    m++;
-  }
-
-  /* correctly normalise the probabilities */
-  for (n = 0; n < nbbd; n++)
-  {
-    if (penorm == 0)
-    {
-      lum[n] = 0.0;
-    }
-    else
-    {
-      eprbs[n] = eprbs[n] / penorm;
-      lum[n] = eprbs[n] * macromain[xplasma->nplasma].matom_emiss[uplvl];
-    }
-    lum_tot += lum[n];
-  }
-
-  return (lum_tot);
 }
